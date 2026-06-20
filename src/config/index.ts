dce3a32d95fff12d52f77b0062375fc16b10d5b6
tony <tony@gmail.com>
2026-06-20 16:53:38 +0700

    add

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,8 @@
 // WS 10001 API 10002 CHAT 10008 CONFIG 10009
 
-export const WS_URL = "wss://www.cliao.one/msg_gateway";
-export const API_URL = "https://www.cliao.one/api";
-export const CHAT_URL = "https://www.cliao.one/chat";
+export const WS_URL = "wss://www.sbtn.shop/msg_gateway";
+export const API_URL = "https://www.sbtn.shop/api";
+export const CHAT_URL = "https://www.sbtn.shop/chat";
 
 // export const WS_URL = "ws://14.29.213.197:50001";
 // export const API_URL = "http://14.29.213.197:50002";
@@ -12,7 +12,7 @@ export const getWsUrl = () => localStorage.getItem("wsUrl") || WS_URL;
 export const getApiUrl = () => localStorage.getItem("apiUrl") || API_URL;
 export const getChatUrl = () => localStorage.getItem("chatUrl") || CHAT_URL;
 
-export const APP_VERSION = "享聊 3.5.1";
+export const APP_VERSION = "三北通 3.5.1";
 export const APP_VERSION_CODE = 350;
 export const SDK_VERSION = "SDK v3.5.1-alpha.3-e-v1.1.4";
 export const CHECK_UPDATE_PREFIX =
